5212789347fb7d1b2cbca3163d97846865dbb448
luan-brav0 <luan-brav0@github.com>
2024-03-24 18:05:21 -0300

    syncing

--- 4-DataStructures/1-LinkedList.c
+++ 4-DataStructures/1-LinkedList.c
@@ -9,7 +9,7 @@ typedef struct Node {
 Node *initNode(int new_data, Node **next_ref) {
   Node *new_node = (Node *)malloc(sizeof(Node));
   if (new_node == NULL) {
-    fprintf(stderr, "Memory allocation error\n");
+    fprintf(stderr, "Error: Memory allocation error\n");
     exit(1);
   }
   new_node->data = new_data;
@@ -30,11 +30,12 @@ int getData() {
 
 void printList(Node **head_ref) {
   if ((*head_ref) == NULL) {
-    fprintf(stderr, "List is empty\n");
+    fprintf(stderr, "Error: List is empty\n");
+    return;
   }
   printf("List items:");
-  for (Node *temp = (*head_ref); temp != NULL; temp = temp->next) {
-    printf(" %d ", temp->data);
+  for (Node *item = (*head_ref); item != NULL; item = item->next) {
+    printf(" %d ", item->data);
   }
   printf("\n");
 }
@@ -44,7 +45,6 @@ void insert(Node **head_ref) {
   Node *new_node = initNode(new_data, head_ref);
   (*head_ref) = new_node;
   printf("%d inserted\n", new_data);
-  printList(head_ref);
 }
 
 void push(Node **head_ref) {
@@ -56,13 +56,12 @@ void push(Node **head_ref) {
     printList(head_ref);
     return;
   }
-  Node *temp = *head_ref;
-  while (temp->next != NULL) {
-    temp = temp->next;
+  Node *last_node = *head_ref;
+  while (last_node->next != NULL) {
+    last_node = last_node->next;
   }
-  temp->next = new_node;
+  last_node->next = new_node;
   printf("%d pushed\n", new_data);
-  printList(head_ref);
 }
 
 void insertAtNthPosition(Node **head_ref) {
@@ -71,9 +70,10 @@ void insertAtNthPosition(Node **head_ref) {
   int position;
   printf("Enter position: ");
   scanf("%d", &position);
-  if (position < 0) {
-    fprintf(stderr,
-            "Invalid position, please try again with a positive value\n");
+  if (position <= 0) {
+    fprintf(
+        stderr,
+        "Error: Invalid position, please try again with a positive value\n");
     return;
   }
   if (position == 1 || (*head_ref) == NULL) {
@@ -82,22 +82,21 @@ void insertAtNthPosition(Node **head_ref) {
     printf("%d inserted at position 1\n", new_data);
     return;
   }
-  Node *temp = *head_ref;
-  if (temp == NULL) {
-    fprintf(stderr, "Invalid position\n");
+  Node *prev_node = *head_ref;
+  if (prev_node == NULL) {
+    fprintf(stderr, "Error: Invalid position\n");
     return;
   }
-  for (int i = 0; i < position - 2 && temp->next != NULL; i++) {
-    temp = temp->next;
+  for (int i = 0; i < position - 2 && prev_node->next != NULL; i++) {
+    prev_node = prev_node->next;
   }
-  new_node->next = temp->next;
-  temp->next = new_node;
-  printList(head_ref);
+  new_node->next = prev_node->next;
+  prev_node->next = new_node;
 }
 
 void reverseList(Node **head_ref) {
   if (*head_ref == NULL || (*head_ref)->next == NULL) {
-    fprintf(stderr, "List is either empty or has only one item\n");
+    fprintf(stderr, "Error: List is either empty or has only one item\n");
     return;
   }
   Node *current = *head_ref;
@@ -110,28 +109,77 @@ void reverseList(Node **head_ref) {
     current = next;
   }
   *head_ref = prev;
-  printList(head_ref);
+}
+
+void deleteNthItem(Node **head_ref) {
+  if (*head_ref == NULL) {
+    fprintf(stderr, "Error: List is empty\n");
+    return;
+  }
+  int position;
+  Node *prev_node = *head_ref;
+  printf("Enter nth position of item to be deleted: ");
+  scanf("%d", &position);
+  if (position < 1) {
+    fprintf(
+        stderr,
+        "Error: Invalid position. Try again with a position bigger than 0\n");
+    return;
+  }
+  if (position == 1) {
+    printf("Deleting 1st item\n");
+    prev_node = *head_ref;
+    *head_ref = prev_node->next;
+    free(prev_node);
+    return;
+  }
+  for (int i = 0; i < position - 2 || prev_node->next == NULL; i++) {
+    if (prev_node->next == NULL) {
+      fprintf(stderr, "Error: Invalid position. Try again\n");
+      return;
+    } else {
+      prev_node = prev_node->next;
+    }
+  }
+
+  printf("Prev node: %d\n", prev_node->data);
+  printf("Deleting %d\n", prev_node->next->data);
+  Node *del_node = prev_node->next;
+  if (del_node->next == NULL) {
+    prev_node->next = NULL;
+  } else {
+    prev_node->next = del_node->next;
+  }
+  free(del_node);
+  printf("Deleted successfully\n");
 }
 
 int main() {
   struct Node *head = NULL;
   printf("List created");
-  int op;
-  const int TOTAL_OPERATIONS = 5;
+  int operation;
+  const int TOTAL_OPERATIONS = 6;
   do {
+    printList(&head);
     printf("Enter operation:"
            "\n\t[1] - Print"
            "\n\t[2] - Insert"
            "\n\t[3] - Push"
            "\n\t[4] - Insert at Nth position"
            "\n\t[5] - Reverse list"
+           "\n\t[6] - Delete Nth item"
            "\n\t[0] - Exit\n");
-    scanf("%d", &op);
-    if (op < 0 || op > TOTAL_OPERATIONS) {
-      fprintf(stderr, "Invalid operation. Try again\n");
+    scanf("%d", &operation);
+    if (operation < 0 || operation > TOTAL_OPERATIONS) {
+      printf("operation: %d\n", operation);
+      fprintf(stderr, "Error: Invalid operation. Try again\n");
       continue;
     }
-    switch (op) {
+    switch (operation) {
+    case 0:
+      printf("Goodbye!\n");
+      return 0;
+      break;
     case 1:
       printList(&head);
       break;
@@ -147,13 +195,13 @@ int main() {
     case 5:
       reverseList(&head);
       break;
-    case 0:
-      printf("Goodbye!\n");
+    case 6:
+      deleteNthItem(&head);
       break;
     default:
-      fprintf(stderr, "Invalid operation. Try again\n");
+      fprintf(stderr, "Error: Invalid operation. Try again\n");
       break;
     }
-  } while (op != 0);
+  } while (1);
   return 0;
 }
