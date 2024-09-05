15023bcc7e913a4b1c45f9e40121e95471d5213e
luan-brav0 <luan-brav0@github.com>
2024-09-04 22:58:01 -0300

    removed debugging logs & minor things

--- 5-DrChuckCBook/5-3_strCat.c
+++ 5-DrChuckCBook/5-3_strCat.c
@@ -11,23 +11,19 @@ char* strCat(char* s, char* t) {
     char* temp = (char*)malloc((sizeof(char) * (strLen(s)+strLen(t)))+1);
     char* pTemp = temp;
     if (temp == NULL) {
-        printf("Memory allocation failed.\n");
+        printf("ERROR: Memory allocation failed.\n");
         return NULL;
     }
     printf("Attempt to concat:\n%s\nand\n%s\n", s, t);
-    for (; *s != '\0'; pTemp++, s++) {
-        printf("%c , %c\n", *pTemp, *s);
+    for (; *s != '\0'; pTemp++, s++)
         *pTemp = *s;
     }
-    for (; *t != '\0'; pTemp++, t++) {
-        printf("%c , %c\n", *pTemp, *t);
+    for (; *t != '\0'; pTemp++, t++)
         *pTemp = *t;
-    }
     return temp;
 }
 
 int main() {
-    // strcat(s,t) -> "s"+"t"
     char* s = "String";
     char* t = " concatenation!";
     char* result = strCat(s, t);
